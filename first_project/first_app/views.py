f17579392887d2eb8406d68f7fea1273b93a00f6
vishal <62096343+imvishal23@users.noreply.github.com>
2021-06-21 22:32:46 +0530

    Added django project file

---
+++ first_project/first_app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# connecting to the database
+from first_app.models import Topic, Webpage,AccessRecord
+
+
+# Create your views here.
+
+def index(request):
+    Webpage_list = AccessRecord.objects.order_by('date')
+    date_dict = {'access_record':Webpage_list}
+    return render(request, 'first_app/index.html', context = date_dict)
